0e66b80b5fa7706f73468f66dd730700b01f7326
yukimoto <shtomcom@gmail.com>
2023-11-22 16:23:34 +0800

    Initial commit

---
+++ apps/near-me/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'near-me',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/near-me',
+};
